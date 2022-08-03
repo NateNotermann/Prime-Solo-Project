fce2c2dd886c1106480966f12ef4c188d78f5ce1
Nate Notermann <nate.notermann@gmail.com>
2022-08-03 14:45:02 -0500

    modified:   src/components/SearchPage/SearchPage.jsx 	modified:   src/redux/reducers/allComedians.reducer.jsx

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -14,18 +14,19 @@ import { useSelector, useDispatch } from 'react-redux'
 
 function SearchPage() {
 
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const listAllComedians = useSelector(store => store.allComediansSaga);
 
-useEffect(() => {
-  dispatch({type: 'GET_ALL_COMEDIANS'});
-}, []);
+  useEffect(() => {
+    dispatch({ type: 'GET_ALL_COMEDIANS' });
+  }, []);
 
 
 
   return (
     <div className="container">
       <p>Search Page</p>
-      
+
 
 
       <div className="inputs">
@@ -35,8 +36,27 @@ useEffect(() => {
         <input placeholder='City Name'></input>
 
       </div>
-        <button>Search Comedians</button>
-        
+      <button>Search Comedians</button>
+      <div>
+        <section className="listAllComedians">
+          {listAllComedians && listAllComedians.map(comedian => {
+            return (
+              <div key={comedian[0].id} >
+                <h3>{comedian.first_name}</h3>
+                <h3>{comedian.last_name}</h3>
+                <img
+                  src={comedian.icon}
+                  alt={comedian.first_name}
+
+                // onClick={(event) => clickPoster (movie)} // sends (movie) to 'clickPoster' function
+                />
+                <h4>{comedian.id}</h4>
+              </div>
+            );
+          })}
+
+        </section>
+      </div>
 
 
 
--- src/redux/reducers/allComedians.reducer.jsx
+++ src/redux/reducers/allComedians.reducer.jsx
@@ -2,7 +2,7 @@
 // --- reducer to save allComedians state --- //
 
 const allComediansReducer =(state = [],  action) => {
-    console.log('allComediansREDUCER (allComedians.jsx)');
+    console.log('allComediansREDUCER (allComedians.jsx) state:', state);
     switch (action.type) {
         case 'SAVE_ALL_COMEDIANS':
             return action.payload;
