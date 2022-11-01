f774b2f4115320e8d942a37b0d930aa353efa61a
Nate Notermann <nate.notermann@gmail.com>
2022-11-01 00:44:04 -0500

    done for night2

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -16,7 +16,7 @@ function AboutPage() {
         <p>Free Joke: What's a developer's least favorite movie?  - A bug's life! </p>
         <p>
           This app was created within a two week sprint during Tier 3 of Prime Digital Academy's Full-Stack Software Engineering program. It was created by Nate Notermann.
-          Please feel free to visit move of his work at the links below, or by scanning the QR code below. 
+          Please feel free to visit move of his work at the links below, or by scanning the QR code below.
           The Technologies used are:
           HTML,
           Javascript,
@@ -32,22 +32,39 @@ function AboutPage() {
           EZ Gif.Com,
           and Apple Shortcuts(QR code).
         </p>
+        <br></br>
+        <div class="linksDiv">
+          <p>
+            <a href='https://www.linkedin.com/in/nate-notermann/' target="_blank" rel="noopener noreferrer"><img
+              alt="linkedin" src="https://upload.wikimedia.org/wikipedia/commons/0/01/LinkedIn_Logo.svg"
+              height='28px' /></a> -
+            Linkedin profile - <a href='https://www.linkedin.com/in/nate-notermann/' target="_blank"
+              rel="noopener noreferrer">LINK</a>
+            <br></br>
 
-       <a href='https://github.com/NateNotermann' className="link"
-          title="Github" target="_blank">Github <span> <a href='https://www.linkedin.com/in/nate-notermann/' className="link"
-          title="Linkedin" target="_blank">Linkedin</a> </span> </a>
-      
-        {/* <a href='https://www.linkedin.com/in/nate-notermann/' className="link"
-          title="Linkedin" target="_blank">Linkedin</a> */}
+            <a href='https://docs.google.com/document/d/1mFvEvQ7kiSoQ_RPUhPihl8GRdw0UNEAm5k25bxaU5Sk/edit?usp=sharing'
+              target="_blank" rel="noopener noreferrer"><img alt="linkedin"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Google_Docs_2020_Logo.svg/800px-Google_Docs_2020_Logo.svg.png"
+                height='28px' /></a> - Resume - <a
+                  href="https://docs.google.com/document/d/1mFvEvQ7kiSoQ_RPUhPihl8GRdw0UNEAm5k25bxaU5Sk/edit?usp=sharing"
+                  target="_blank" rel="noopener noreferrer">LINK</a>
+            <br></br>
+            <a href="mailto: nate.notermann@gmail.com" target="_blank" rel="noopener noreferrer"><img alt="linkedin"
+              src="https://1000logos.net/wp-content/uploads/2018/05/Gmail-Logo-2013.png" height='28px' /></a> - Don't
+            hesitate to reach out via Email - <a href="mailto: nate.notermann@gmail.com">LINK</a>
+          </p>
+        </div>
+
+   
         <p>
           Special thanks to family and friends.
           I'd also like to all of the staff at prime Digital Academy and everyone in the Jemisin cohort.
           EXTRA SPECIAL Thanks to my instructors Liz and Dane!
         </p>
 
-          <div className="logo"></div>
-          <div className="githubQR"></div>
-        
+        <div className="logo"></div>
+        <div className="githubQR"></div>
+
       </div>
     </center>
   </>
